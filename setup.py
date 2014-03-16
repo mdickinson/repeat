f189e1da2d10e3517e8191b2147ac4624da32b2b
Mark Dickinson <mdickinson@enthought.com>
2014-03-16 11:38:50 +0000

    Add tests and make compatible with other Python versions.  - add some basic unit tests  - add progress_stream parameter to the core command  - make compatible with Python 2.6+ and Python 3.2+

--- setup.py
+++ setup.py
@@ -1,11 +1,32 @@
+import sys
+
 from setuptools import setup
+
+
+# Required packages.  argparse was introduced into the standard library
+# in Python 2.7; mock was introduced in Python 3.3.
+required_packages = ["six"]
+if sys.version_info < (2, 7):
+    required_packages.append("argparse")
+if sys.version_info < (3, 3):
+    required_packages.append("mock")
+
 
 setup(
     name='repeat',
+    version='0.1.0',
+    author="Mark Dickinson",
+    author_email="dickinsm@gmail.com",
+    url="https://github.com/mdickinson/repeat",
+    license="Apache license",
+    description=(
+        "Repeat a command indefinitely or for a fixed number of iterations"),
     py_modules=['repeat'],
+    platforms=["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
     entry_points={
         'console_scripts': [
             'repeat = repeat:main',
         ],
     },
+    install_requires=required_packages,
 )
