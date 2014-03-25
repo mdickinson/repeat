0661c5d471ff759702c687c6343a723e657d9505
mark.dickinson <mdickinson@enthought.com>
2014-03-25 12:19:52 +0000

    Move mocking of subprocess.call into test setUp.

--- test_repeat.py
+++ test_repeat.py
@@ -21,12 +21,22 @@ import six
 import repeat
 
 
-class MockCheckCall(object):
+class MockSubprocessCall(object):
     def __init__(self, returncodes=[]):
         self.returncodes = itertools.chain(returncodes, itertools.repeat(0))
+        self.call_args_list = []
 
-    def __call__(self, cmd):
+    def __call__(self, *args, **kwargs):
+        self.call_args_list.append((args, kwargs))
         return next(self.returncodes)
+
+    @property
+    def call_count(self):
+        return len(self.call_args_list)
+
+    @property
+    def called(self):
+        return self.call_count > 0
 
 
 class TestRepeat(unittest.TestCase):
@@ -37,59 +47,57 @@ class TestRepeat(unittest.TestCase):
     def setUp(self):
         self.mock_stdout = io.StringIO()
         self.mock_cmd = ['do', 'something']
+        patcher = mock.patch('repeat.subprocess.call', MockSubprocessCall())
+        self.mock_subprocess_call = patcher.start()
+        self.addCleanup(patcher.stop)
 
     def tearDown(self):
         pass
 
     def test_repeat_ten_times(self):
-        with mock.patch('repeat.subprocess.call') as m:
-            m.side_effect = MockCheckCall()
-            returncode = repeat.repeat(
-                self.mock_cmd, count=10, progress_stream=self.mock_stdout)
+        returncode = repeat.repeat(
+            self.mock_cmd, count=10, progress_stream=self.mock_stdout)
         self.assertEqual(returncode, 0)
-        self.assertEqual(m.call_count, 10)
-        for args, kwargs in m.call_args_list:
+        self.assertEqual(self.mock_subprocess_call.call_count, 10)
+        for args, kwargs in self.mock_subprocess_call.call_args_list:
             self.assertEqual(len(args), 1)
             self.assertEqual(args[0], self.mock_cmd)
             self.assertFalse(kwargs)
 
     def test_repeat_zero_times(self):
-        with mock.patch('repeat.subprocess.call') as m:
-            returncode = repeat.repeat(
-                self.mock_cmd, count=0, progress_stream=self.mock_stdout)
+        returncode = repeat.repeat(
+            self.mock_cmd, count=0, progress_stream=self.mock_stdout)
         self.assertEqual(returncode, 0)
-        self.assertFalse(m.called)
+        self.assertFalse(self.mock_subprocess_call.called)
 
     def test_abort_on_failure(self):
-        with mock.patch('repeat.subprocess.call') as m:
-            m.side_effect = MockCheckCall(returncodes=[0, 7])
-            returncode = repeat.repeat(
-                self.mock_cmd, count=10, progress_stream=self.mock_stdout)
+        self.mock_subprocess_call.returncodes = iter([0, 7])
+        returncode = repeat.repeat(
+            self.mock_cmd, count=10, progress_stream=self.mock_stdout)
         self.assertEqual(returncode, 1)
-        self.assertEqual(m.call_count, 2)
+        self.assertEqual(self.mock_subprocess_call.call_count, 2)
 
     def test_no_progress_output_when_verbose_is_false(self):
-        with mock.patch('repeat.subprocess.call'):
-            repeat.repeat(
-                cmd=self.mock_cmd,
-                count=10,
-                verbose=False,
-                progress_stream=self.mock_stdout,
-            )
+        returncode = repeat.repeat(
+            cmd=self.mock_cmd,
+            count=10,
+            verbose=False,
+            progress_stream=self.mock_stdout,
+        )
+        self.assertEqual(returncode, 0)
+        self.assertEqual(self.mock_subprocess_call.call_count, 10)
         output = self.mock_stdout.getvalue()
         self.assertEqual(output, '')
 
     def test_prefix_in_progress_output(self):
         prefix = 'test_prefix: '
-        with mock.patch('repeat.subprocess.call') as m:
-            m.side_effect = MockCheckCall()
-            repeat.repeat(
-                cmd=self.mock_cmd,
-                count=10,
-                verbose=True,
-                progress_stream=self.mock_stdout,
-                prefix=prefix,
-            )
+        repeat.repeat(
+            cmd=self.mock_cmd,
+            count=10,
+            verbose=True,
+            progress_stream=self.mock_stdout,
+            prefix=prefix,
+        )
         output = self.mock_stdout.getvalue().splitlines(True)
         # There should be at least *some* output.
         self.assertGreater(len(output), 0)
@@ -98,15 +106,13 @@ class TestRepeat(unittest.TestCase):
             self.assertTrue(line.endswith('\n'))
 
     def test_reported_runs_in_progress_output(self):
-        with mock.patch('repeat.subprocess.call') as m:
-            m.side_effect = MockCheckCall()
-            repeat.repeat(
-                cmd=self.mock_cmd,
-                count=3,
-                verbose=True,
-                progress_stream=self.mock_stdout,
-                prefix='testing:',
-            )
+        repeat.repeat(
+            cmd=self.mock_cmd,
+            count=3,
+            verbose=True,
+            progress_stream=self.mock_stdout,
+            prefix='testing:',
+        )
         full_output = self.mock_stdout.getvalue()
         expected_run_reporting = textwrap.dedent("""\
             testing:Starting run 1 of 3.
@@ -119,15 +125,14 @@ class TestRepeat(unittest.TestCase):
         self.assertIn(expected_run_reporting, full_output)
 
     def test_reported_runs_in_infinite_case(self):
-        with mock.patch('repeat.subprocess.call') as m:
-            m.side_effect = MockCheckCall(returncodes=[0, 0, -23])
-            repeat.repeat(
-                cmd=self.mock_cmd,
-                count=None,
-                verbose=True,
-                progress_stream=self.mock_stdout,
-                prefix='infinite testing> ',
-            )
+        self.mock_subprocess_call.returncodes = iter([0, 0, -23])
+        repeat.repeat(
+            cmd=self.mock_cmd,
+            count=None,
+            verbose=True,
+            progress_stream=self.mock_stdout,
+            prefix='infinite testing> ',
+        )
         full_output = self.mock_stdout.getvalue()
         expected_run_reporting = textwrap.dedent("""\
             infinite testing> Starting run 1.
